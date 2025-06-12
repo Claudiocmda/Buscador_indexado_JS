9219636d5e92c3fb11595eaed4e970d6a658cc69
Claudiocmda <claudioanaquin150@gmail.com>
2025-06-12 16:41:53 -0300

    Arreglar el puerto para produccion

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { trie } = require('./db');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
